79288ef2e4155790480e750d1293d8dd83468015
Marc <marriclop5@alu.edu.gva.es>
2025-01-17 09:49:45 +0100

    Todo

---
+++ src/main/java/com/openwebinars/spring/PlumatintaApplication.java
@@ -0,0 +1,13 @@
+package com.openwebinars.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumatintaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumatintaApplication.class, args);
+	}
+
+}
